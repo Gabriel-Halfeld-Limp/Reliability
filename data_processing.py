3ff93e3ac93857ac6442edafd250182d0597ed95
Gabriel-Halfeld-Limp <halfeld.limp.gabriel@gmail.com>
2024-12-15 21:26:39 -0300

    Monte Carlo Sequencial (#1)
    * teste_print
    
    teste
    
    * teste2
    
    teste2
    
    * Término do algoritmo principal
    
    * Simulação maior feita
    
    * relatório SMCSeq e comentários feitos
    
    * Update monte_carlo_Seq.ipynb
    
    .

--- data_processing.py
+++ data_processing.py
@@ -1,11 +1,20 @@
+# Importando a biblioteca pandas para manipulação de dados tabulares
 import pandas as pd
 
+# Função para carregar todos os dados necessários a partir de arquivos CSV
 def load_all_data():
+    # Carregando o arquivo 'D_GEN.csv' que contém dados sobre geradores de energia
     D_GEN = pd.read_csv('DADOS/D_GEN.csv')
+    
+    # Carregando o arquivo 'D_LIN.csv' que contém dados sobre linhas de transmissão
     D_LIN = pd.read_csv('DADOS/D_LIN.csv')
+    
+    # Carregando o arquivo 'D_LOAD.csv' que contém dados sobre as cargas de energia
     D_LOAD = pd.read_csv('DADOS/D_LOAD.csv')
+    
+    # Retornando os dados carregados como um dicionário, com as chaves representando os tipos de dados
     return {
-        'D_GEN': D_GEN,
-        'D_LIN': D_LIN,
-        'D_LOAD': D_LOAD
+        'D_GEN': D_GEN,   # Dados dos geradores
+        'D_LIN': D_LIN,   # Dados das linhas de transmissão
+        'D_LOAD': D_LOAD  # Dados das cargas de energia
     }
